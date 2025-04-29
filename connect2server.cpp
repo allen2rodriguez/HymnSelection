fe9d78a91dd830ec1ae338e18c6397ff61793217
Allen Rodriguez <aerodr26@asu.edu>
2025-04-28 21:58:51 -0700

    Add in the neccessary include files for MySQL

---
+++ connect2server.cpp
@@ -0,0 +1,11 @@
+//g++ main.cpp -o myprogram -lmysqlcppconn
+
+#include <iostream>
+#include <stdexcept>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
+#include <cppconn/exception.h>
+
+
